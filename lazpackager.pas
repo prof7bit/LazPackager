3aaf94369a32580e1c0c80a845f18355bfee66d0
prof7bit <prof7bit@googlemail.com>
2012-10-03 11:55:15 +0200

    rename package lazdebian.lpk -> lazpackager.lpk

--- lazpackager.pas
+++ lazpackager.pas
@@ -2,7 +2,7 @@
   This source is only used to compile and install the package.
  }
 
-unit lazdebian;
+unit LazPackager;
 
 interface
 
@@ -19,5 +19,5 @@ begin
 end;
 
 initialization
-  RegisterPackage('lazdebian', @Register);
+  RegisterPackage('LazPackager', @Register);
 end.
